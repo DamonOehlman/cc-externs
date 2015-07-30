475ad81055c2388a84e5a6a79f83c1a5a06fc8ed
Damon Oehlman <damon.oehlman@gmail.com>
2015-07-30 15:56:32 +1000

    progress on tweaking runtests.sh - fighting with pipefail :/

--- runtests.sh
+++ runtests.sh
@@ -1,42 +1,61 @@
 #!/usr/bin/env bash
 
-# set exit on error and tracing
-# see https://gist.github.com/wolfeidau/6761104#file-pi-build-deb-sh-L3-L4
-# set -e
-# set -o errtrace
+# strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
 shopt -s nullglob
+IFS=$'\n\t'
+
+# define the --jscomp_error flags we want to use
+JSCOMP_ERRORS=(
+	accessControls
+	checkRegExp
+	checkTypes
+	uselessCode
+	checkVars
+	const
+	globalThis
+	nonStandardJsDocs
+	missingProperties
+	strictModuleDepCheck
+	suspiciousCode
+	undefinedNames
+	visibility
+)
+
+# define the --jscomp_warning flags
+JSCOMP_WARNINGS=(
+	deprecated
+)
+
+function genClosureArgs() {
+	ARGS=()
+	for flag in ${JSCOMP_ERRORS[@]}; do
+		ARGS+=(--jscomp_error $flag)
+	done
+
+	for flag in ${JSCOMP_WARNINGS[@]}; do
+		ARGS+=(--jscomp_warning $flag)
+	done
+
+	ARGS+=(--language_in ECMASCRIPT5)
+	ARGS+=(--charset UTF-8)
+	ARGS+=(--warning_level VERBOSE)
+	ARGS+=(--summary_detail_level 3)
+
+	echo ${ARGS[@]}
+}
 
 TARGET_EXTERN=$1
 PATH_TESTS=test/$TARGET_EXTERN
+CLOSURE_JAR="node_modules/google-closure-compiler/compiler.jar"
 
-function testCompile {
-	echo "testing $1"
-
-	java -jar node_modules/google-closure-compiler/compiler.jar \
-		--jscomp_error accessControls \
-		--jscomp_error checkRegExp \
-		--jscomp_error checkTypes \
-		--jscomp_error uselessCode \
-		--jscomp_error checkVars \
-		--jscomp_error const \
-		--jscomp_error globalThis \
-		--jscomp_error nonStandardJsDocs \
-		--jscomp_error missingProperties \
-		--jscomp_error strictModuleDepCheck \
-		--jscomp_error suspiciousCode \
-		--jscomp_error undefinedNames \
-		--jscomp_error visibility \
-		--jscomp_warning deprecated \
-		--language_in ECMASCRIPT5 \
-		--charset UTF-8 \
-		--warning_level VERBOSE \
-		--summary_detail_level 3 \
-		--externs $TARGET_EXTERN.js \
-		--js $1
-}
+echo "running bash $BASH_VERSION"
+echo -e ">>> running tests for $1.js externs"
 
 for testfile in $PATH_TESTS/pass/*.js; do
-	testCompile $testfile
+	echo -e "\n>>> checking $testfile"
+	java -jar $CLOSURE_JAR $(genClosureArgs)[@] --externs $TARGET_EXTERN.js --js $testfile > /dev/null || true
+
 	if [ $? -ne 0 ]; then
 		echo "Captured FAIL for expected valid js: $testfile"
 		exit 1;
@@ -44,7 +63,10 @@ for testfile in $PATH_TESTS/pass/*.js; do
 done
 
 for testfile in $PATH_TESTS/fail/*.js; do
-	testCompile $testfile
+	echo -e "\n>>> checking $testfile"
+	java -jar $CLOSURE_JAR $(genClosureArgs)[@] --externs $TARGET_EXTERN.js --js $testfile > /dev/null || true
+	echo "errorlevel = $?"
+
 	if [ $? -eq 0 ]; then
 		echo "Captured PASS for expected invalid test: $testfile"
 		exit 1;
