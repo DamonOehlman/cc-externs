5063d3cd43441be8cb2b61d6112d4d175d69baac
Damon Oehlman <damon.oehlman@gmail.com>
2015-08-16 19:32:10 +1000

    Disable mousetrap.js extern for now ("bind" is bad)

--- disabled/mousetrap.js
+++ disabled/mousetrap.js
@@ -1,32 +1,32 @@
 /* Mousetrap Constructor and Instance Methods */
 
 /**
-  @constructor
-  @param {!HTMLElement} targetElement
-*/
+ * @constructor
+ * @param {!HTMLElement} targetElement
+ */
 function Mousetrap(targetElement) {}
 
 /**
-  @param {!MousetrapKeySpec} keys
-  @param {!MousetrapKeyHandler} callback
-  @param {string=} action
-  @return {!Mousetrap}
-*/
+ * @param {!MousetrapKeySpec} keys
+ * @param {!MousetrapKeyHandler} callback
+ * @param {string=} action
+ * @return {!Mousetrap}
+ */
 Mousetrap.prototype.bind = function(keys, callback, action) {};
 
 /* Mousetrap Static Functions */
 
 /**
-  @param {!MousetrapKeySpec} keys
-  @param {!MousetrapKeyHandler} callback
-  @param {string=} action
-  @return {!Mousetrap}
-*/
+ * @param {!MousetrapKeySpec} keys
+ * @param {!MousetrapKeyHandler} callback
+ * @param {string=} action
+ * @return {!Mousetrap}
+ */
 Mousetrap.bind = function(keys, callback, action) {};
 
 /**
-  @param {!MousetrapKeySpec} keys
-**/
+ * @param {!MousetrapKeySpec} keys
+ */
 Mousetrap.unbind = function(keys) {};
 
 /* TYPEDEFS */
