85bb11d4fd8697a1e075e5c078fb1fdd0f03869b
Damon Oehlman <damon.oehlman@gmail.com>
2015-08-16 19:32:59 +1000

    formatting tweaks

--- eve.js
+++ eve.js
@@ -2,13 +2,13 @@
  @param {string} name
  @param {Object?} scope
  @param {...*} args
-*/
+ */
 function eve(name, scope, args) {}
 
 /**
  @param {string} name
  @param {!EveHandler} handler
-**/
+ */
 eve.on = function(name, handler) {};
 
 /* typedefs */
