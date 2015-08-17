e2767b8bc345ecdb120d2e1dbcdea88de1b2cdf8
Damon Oehlman <damon.oehlman@gmail.com>
2015-08-17 12:15:42 +1000

    Use hardnosed-closure

--- runtests.sh
+++ runtests.sh
@@ -5,56 +5,14 @@ set -uo pipefail
 shopt -s nullglob
 IFS=$'\n\t'
 
-# define the --jscomp_error flags we want to use
-JSCOMP_ERRORS=(
-	accessControls
-	checkRegExp
-	checkTypes
-	uselessCode
-	checkVars
-	const
-	globalThis
-	nonStandardJsDocs
-	missingProperties
-	strictModuleDepCheck
-	suspiciousCode
-	undefinedNames
-	visibility
-)
-
-# define the --jscomp_warning flags
-JSCOMP_WARNINGS=(
-	deprecated
-)
-
-function genClosureArgs() {
-	ARGS=()
-	for flag in ${JSCOMP_ERRORS[@]}; do
-		ARGS+=(--jscomp_error $flag)
-	done
-
-	for flag in ${JSCOMP_WARNINGS[@]}; do
-		ARGS+=(--jscomp_warning $flag)
-	done
-
-	ARGS+=(--language_in ECMASCRIPT5)
-	ARGS+=(--charset UTF-8)
-	ARGS+=(--warning_level VERBOSE)
-	ARGS+=(--summary_detail_level 3)
-	ARGS+=(--compilation_level ADVANCED_OPTIMIZATIONS)
-
-	echo "${ARGS[*]}"
-}
-
 TARGET_EXTERN=$1
 PATH_TESTS=test/$TARGET_EXTERN
-CLOSURE_JAR="node_modules/google-closure-compiler/compiler.jar"
 
 echo -e "\n=== $1.js ==="
 
 for testfile in $PATH_TESTS/pass/*.js; do
 	echo -e "\n>>> checking $testfile"
-	java -jar $CLOSURE_JAR $(genClosureArgs) --externs *.js --js $testfile > /dev/null
+	$(npm bin)/hardnosed-closure --externs *.js --js $testfile > /dev/null
 
 	if [ $? -ne 0 ]; then
 		echo "Captured FAIL for expected valid js: $testfile"
@@ -64,7 +22,7 @@ done
 
 for testfile in $PATH_TESTS/fail/*.js; do
 	echo -e "\n>>> checking $testfile"
-	java -jar $CLOSURE_JAR $(genClosureArgs) --externs $TARGET_EXTERN.js --js $testfile > /dev/null 2>&1
+	$(npm bin)/hardnosed-closure --externs $TARGET_EXTERN.js --js $testfile > /dev/null 2>&1
 
 	if [ $? -eq 0 ]; then
 		echo "Captured PASS for expected invalid test: $testfile"
